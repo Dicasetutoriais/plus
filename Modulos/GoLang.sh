5a5071245feb3ed0ad3cf557607f29abea6269bb
PhoenixxZ2023 <101994539+PhoenixxZ2023@users.noreply.github.com>
2024-02-04 15:48:49 -0300

    Update GoLang.sh

--- Modulos/GoLang.sh
+++ Modulos/GoLang.sh
@@ -1,62 +1,73 @@
 #!/bin/bash
-#30/03/2022
+# Date: 30/03/2022
 
-removego () {
-rm -rf /usr/local/go
-rm -rf $HOME/go
-killall go 1> /dev/null 2> /dev/null
+remove_go() {
+    # Remove existing Go installations
+    rm -rf /usr/local/go
+    rm -rf "$HOME/go"
+    killall go &>/dev/null
 }
 
-#funtion
-goinst () {
-tput clear
-echo -e "\033[1;33m   INSTALADOR GO Lang (Multi Protocolo) \033[0m"
-echo
-echo -e "\033[1;33m A continuacion se instalara el paquete GO Lang\n Que es Base de Varios Protocolos... \033[0m"
-echo -e "\033[1;33m Continuar? \033[0m"
-while [[ ${yesno} != @(s|S|y|Y|n|N) ]]; do
-read -p " [S/N]: " yesno
-tput cuu1 && tput dl1
-done
-if [[ ${yesno} = @(s|S|y|Y) ]]; then
-echo -e "\033[1;32m Instalando...\033[0m"
-cd
-rm -rf /usr/local/go 1> /dev/null 2> /dev/null
-wget https://golang.org/dl/go1.15.linux-amd64.tar.gz 1> /dev/null 2> /dev/null
-tar -C /usr/local -xzf go1.15.linux-amd64.tar.gz
-if cat /root/.bashrc | grep GOROOT; then
-echo -e "\033[1;31m Ja Existe um GoROOT - Skipping\033[0m"
-else
-sed -i '$a export GOROOT=/usr/local/go' /root/.bashrc
-sed -i '$a export GOPATH=$HOME/go' /root/.bashrc
-sed -i '$a export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' /root/.bashrc
-rm go1.15.linux-amd64.tar.gz
-source /root/.bashrc
-fi
-sleep 3
-echo -e "\033[1;32m Reiniciando Fonte do Terminal... \033[0m"
-fi
-echo -e "\033[1;33m Para finalizar o processo de Instalacion de GO escribe :\033[0m"
-echo -e "\033[1;32m source ~/.profile\033[0m"
+install_go() {
+    tput clear
+    echo -e "\033[1;33m   GO Lang Installer (Multi-Protocol) \033[0m"
+    echo
+    echo -e "\033[1;33m This will install the GO Lang package,\n which serves as the base for various protocols. \033[0m"
+    echo -e "\033[1;33m Continue? \033[0m"
+
+    read -p " [S/N]: " yesno
+    tput cuu1 && tput dl1
+
+    if [[ "${yesno,,}" == "y" ]]; then
+        echo -e "\033[1;32m Installing... \033[0m"
+        cd || exit
+
+        # Download and extract Go
+        wget -q https://golang.org/dl/go1.15.linux-amd64.tar.gz
+        tar -C /usr/local -xzf go1.15.linux-amd64.tar.gz
+
+        # Check if GOROOT is already set in .bashrc
+        if grep -q GOROOT /root/.bashrc; then
+            echo -e "\033[1;31m GoROOT already exists - Skipping \033[0m"
+        else
+            # Add GOROOT, GOPATH, and PATH to .bashrc
+            cat <<EOF >> /root/.bashrc
+export GOROOT=/usr/local/go
+export GOPATH=\$HOME/go
+export PATH=\$GOPATH/bin:\$GOROOT/bin:\$PATH
+EOF
+            rm go1.15.linux-amd64.tar.gz
+            source /root/.bashrc
+        fi
+
+        sleep 3
+        echo -e "\033[1;32m Restarting the Terminal... \033[0m"
+    fi
+
+    echo -e "\033[1;33m To complete the GO installation process, run: \033[0m"
+    echo -e "\033[1;32m source ~/.profile \033[0m"
 }
 
-gomenu () {
-tput clear
-echo -e "\033[1;33m     PACK GO Lang INSTALLER    \033[1;33 \033[0m"
-echo -e
-echo -e "\033[1;32m  [1] \033[1;33mINSTALAR PACK GO Lang \033[0m"
-echo -e "\033[1;32m  [2] \033[1;33mDESINSTALAR GO Lang \033[0m"
-echo -e "\033[1;33m ▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎ \033[0m"
-echo -e "\033[1;32m  [0] \033[1;33m SAIR \033[0m"
-echo -e
-read -p "ESCOLHA UMA OPÇÃO: " opcion
-case $opcion in
-0)exit;;
-1)goinst;;
-2)removego;;
-*)echo -e "\033[1;31m\n ESCOLHA UMA OPÇÃO VALIDA...!!! \033[0m"
-sleep 1
-gomenu;;
-esac
+go_menu() {
+    tput clear
+    echo -e "\033[1;33m     GO Lang Installer    \033[0m"
+    echo -e
+    echo -e "\033[1;32m  [1] \033[1;33mInstall GO Lang \033[0m"
+    echo -e "\033[1;32m  [2] \033[1;33mUninstall GO Lang \033[0m"
+    echo -e "\033[1;33m ▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎▪︎ \033[0m"
+    echo -e "\033[1;32m  [0] \033[1;33mEXIT \033[0m"
+    echo -e
+    read -p "CHOOSE AN OPTION: " option
+
+    case $option in
+        0) exit ;;
+        1) install_go ;;
+        2) remove_go ;;
+        *)
+            echo -e "\033[1;31m\n CHOOSE A VALID OPTION...!!! \033[0m"
+            sleep 1
+            go_menu ;;
+    esac
 }
-gomenu
+
+go_menu
